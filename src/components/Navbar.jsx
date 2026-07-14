26ab04649e7536ddfca78715dc9dfadc67551ff0
Sush-psusmitha <sushsusmitha13@gmail.com>
2026-07-14 20:16:36 +0530

    test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import assets from '../assets/assets'
+import ThemeToggleBtn from './ThemeToggleBtn';
+
 const Navbar = ({theme,setTheme}) => {
 
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -11,13 +13,15 @@ const Navbar = ({theme,setTheme}) => {
     <div className={`text-gray-700 dark:text-white sm:text-sm ${  !sidebarOpen ? "max-sm:w-0 overflow-hidden" : "max-sm:w-60 max-sm:pl-10"} max-sm:fixed top-0 bottom-0 right-0 max-sm:min-h-screen max-sm:h-full max-sm:flex-col max-sm:bg-primary max-sm:text-white max-sm:pt-20 flex sm:items-center gap-5 transition-all`}>
 
         <img src={assets.close_icon} alt=""  className='w-5 absolute right-4 top-4 sm:hidden' onClick={()=> setSidebarOpen(false)}/>
-        <a href="#" className='sm:hover:border-b'>Home</a>
-        <a href="#services" className='sm:hover:border-b'>Services</a>
-        <a href="#our-work" className='sm:hover:border-b'>Our Work</a>
-        <a href="#contact-us" className='sm:hover:border-b'>Contact Us</a>
+        <a onClick={()=>setSidebarOpen(false)} href="#" className='sm:hover:border-b'>Home</a>
+        <a onClick={()=>setSidebarOpen(false)} href="#services" className='sm:hover:border-b'>Services</a>
+        <a onClick={()=>setSidebarOpen(false)} href="#our-work" className='sm:hover:border-b'>Our Work</a>
+        <a onClick={()=>setSidebarOpen(false)} href="#contact-us" className='sm:hover:border-b'>Contact Us</a>
       </div>
 
       <div className='flex items-center gap-2 sm:gap-4'>
+
+        <ThemeToggleBtn theme={theme} setTheme={setTheme} />
         <img src={theme === 'dark'? assets.menu_icon_dark: assets.menu_icon} alt="" onClick={()=>setSidebarOpen(true)} className='w-8 sm:hidden'/>
         <a href="#contact-us" className='text-sm max-sm:hidden flex items-center gap-2 bg-primary text-white px-6 py-2 rounded-full cursor-pointer hover:scale-103 transition-all'>
           connect <img src={assets.arrow_icon} width={14} />
